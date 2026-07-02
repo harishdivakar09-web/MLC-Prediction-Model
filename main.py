58c91f3e9095282b24c2c4d15e290d0e6c48d190
Harish Divakar <harish.divakar.09@gmail.com>
2026-07-02 12:35:20 -0400

    Modified dataframe and normalized data for team stats

--- main.py
+++ main.py
@@ -6,9 +6,33 @@ all_rounder_raw = pd.read_csv('all_rounders.csv')
 bowling_raw = pd.read_csv('bowlers.csv')
 team_raw = pd.read_csv('team_season.csv')
 
-#Team score
-print(team_raw.head())
-team_score = 2
+#Modification of team.season.csv to include win percentage and dropping wins and losses columns
+team_raw['win_percentage_2023'] = round((team_raw['wins_2023']/(team_raw['wins_2023']+team_raw['losses_2023']))*100,4)
+team_raw['win_percentage_2024'] = round((team_raw['wins_2024']/(team_raw['wins_2024']+team_raw['losses_2024']))*100,4)
+team_raw['win_percentage_2025'] = round((team_raw['wins_2025']/(team_raw['wins_2025']+team_raw['losses_2025']))*100,4)
+team_mod = team_raw.drop(columns = ['wins_2023', 'losses_2023','wins_2024','losses_2024','wins_2025','losses_2025'])
 
 
+#Normalization of NRR, Win Percentage, and Standings
+team_mod['nrr_2023'] = round((team_mod['nrr_2023']-team_mod['nrr_2023'].min())/(team_mod['nrr_2023'].max()-team_mod['nrr_2023'].min()),4)
+team_mod['nrr_2024'] = round((team_mod['nrr_2024']-team_mod['nrr_2024'].min())/(team_mod['nrr_2024'].max()-team_mod['nrr_2024'].min()),4)
+team_mod['nrr_2025'] = round((team_mod['nrr_2025']-team_mod['nrr_2025'].min())/(team_mod['nrr_2025'].max()-team_mod['nrr_2025'].min()),4)
+team_mod['win_percentage_2023'] = round((team_mod['win_percentage_2023']-team_mod['win_percentage_2023'].min())/(team_mod['win_percentage_2023'].max()-team_mod['win_percentage_2023'].min()),4)
+team_mod['win_percentage_2024'] = round((team_mod['win_percentage_2024']-team_mod['win_percentage_2024'].min())/(team_mod['win_percentage_2024'].max()-team_mod['win_percentage_2024'].min()),4)
+team_mod['win_percentage_2025'] = round((team_mod['win_percentage_2025']-team_mod['win_percentage_2025'].min())/(team_mod['win_percentage_2025'].max()-team_mod['win_percentage_2025'].min()),4)
 
+standing_points= {
+    'Group' : 0.0,
+    '4th' : 0.25,
+    '3rd' : 0.5,
+    'Runner Up' : 0.75,
+    'Winner' : 1.0
+}
+
+team_mod['standings_2023'] = team_mod['standings_2023'].str.strip().map(standing_points)
+team_mod['standings_2024'] = team_mod['standings_2024'].str.strip().map(standing_points)
+team_mod['standings_2025'] = team_mod['standings_2025'].str.strip().map(standing_points)
+
+#Weight assignment 
+
+print(team_mod.head())
